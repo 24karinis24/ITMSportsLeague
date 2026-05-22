08d85d242ac78b89648ed18e1657e176fef3ab3e
Karinis24 <evelynkarinah09@gmail.com>
2026-05-22 00:30:07 -0500

    Evaluative Event 4 - Creation and configuration of MatchLineup and Swagger Tests

--- SportsLeague.API/Mappings/MappingProfile.cs
+++ SportsLeague.API/Mappings/MappingProfile.cs
@@ -70,6 +70,5 @@ public class MappingProfile : Profile
             .ForMember(dest => dest.PlayerName,
                 opt => opt.MapFrom(src =>
                     src.Player.FirstName + " " + src.Player.LastName));
-
     }
 }
